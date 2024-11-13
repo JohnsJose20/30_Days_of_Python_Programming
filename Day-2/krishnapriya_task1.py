8252e1a0641ad8736207dc03b30b2341b6c0d99a
Krishnapriya <krishnapriyaar292@gmail.com>
2024-11-13 20:19:49 +0530

    Add files via upload

---
+++ Day-2/krishnapriya_task1.py
@@ -0,0 +1,12 @@
+'''
+Author:Krishnapriya
+Date:13/11/2024
+Description:Arithmetic operations to perform basic math operators like addition,subtraction,and multiplication.and division.
+'''
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
