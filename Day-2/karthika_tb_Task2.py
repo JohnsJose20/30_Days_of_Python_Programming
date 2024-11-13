e8691187de0da5f6f838ee7a0c2f28239476f09a
karthikatb <karthikatb20@gmail.com>
2024-11-13 20:30:59 +0530

    Add files via upload

---
+++ Day-2/karthika_tb_Task2.py
@@ -0,0 +1,10 @@
+#Author:Karthika T.B
+#Date:13-11-2024
+#Description: In this program, we will check whether a given number is positive, negative, or zero
+num=float(input("Enter a number:"))
+if num>0:
+    print("The number is positive.")
+elif num<0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
