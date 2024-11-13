70c3c6e88afb7d8a7cbafab5572cc2c61e7128bf
joubertjose <joubertjose200614@gmail.com>
2024-11-13 21:29:03 +0530

    Add files via upload

---
+++ Day-2/joubert_jose_task2.py
@@ -0,0 +1,10 @@
+#Task 2:
+# Asking the user to input a number
+num = float(input("Enter a number: "))
+# Checking if the number is positive, negative, or zero
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
