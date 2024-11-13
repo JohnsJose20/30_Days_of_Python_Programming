b3278e0dca9cf8ef19841675f15cce0805985ffc
Kritthik2609 <kritthik2609@gmail.com>
2024-11-13 22:46:03 +0530

    Add files via upload

---
+++ Day-2/KritthikTask1.py
@@ -0,0 +1,18 @@
+"""
+Author: Kritthik
+Date: 13-11-2024
+"""
+a = 10
+b = 5
+
+Addition = a + b
+print("a + b is: ",Addition)
+
+Subtraction = a - b
+print("a - b is: ",Subtraction)
+
+Multiplication = a * b
+print("a multiplied with b is: ",Multiplication)
+
+Division = a / b
+print("a divided by b is: ", Division)
