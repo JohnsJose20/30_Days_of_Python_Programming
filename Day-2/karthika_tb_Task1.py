3f4c8ed72e873bccd7cdfbfb788c05c1aae6e950
karthikatb <karthikatb20@gmail.com>
2024-11-13 20:16:34 +0530

    Add files via upload

---
+++ Day-2/karthika_tb_Task1.py
@@ -0,0 +1,11 @@
+#Author:karthika T.B
+#Date:13-11-2024
+#Description:In Python, you can use arithmetic operators to perform basic math operations like addition,
+# subtraction, multiplication, and division. These operators can be applied to numbers directly or variables
+# holding numerical values.
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
