d11038acc3a839046eac161136811b98275d8e9a
Krishnapriya <krishnapriyaar292@gmail.com>
2024-11-13 20:36:38 +0530

    Add files via upload

---
+++ Day-2/krishnapriya_task2.py
@@ -0,0 +1,12 @@
+'''
+Author:Krishnapriya A R
+Date :13/11/2024
+Description:In this program we will check whether a given number is positive,negative or zero
+'''
+num=float(input("Enter a number: "))
+if num>0:
+    print("The number is positive.")
+elif num<0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
