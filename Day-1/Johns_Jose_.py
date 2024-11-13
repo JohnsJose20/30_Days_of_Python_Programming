f237cd98a5490e13c986038072ab3d9922bdfc2e
Johns Jose <jjkanichukattu@gmail.com>
2024-11-13 22:31:12 +0530

    Add files via upload

---
+++ Day-1/Johns_Jose_.py
@@ -0,0 +1,11 @@
+'''
+Author: Johns Jose
+Date:12/11/2024
+Description:The print() function prints the specified 
+message to the screen or other standard output devices.
+The message can be a string or any other object;
+the object will be converted into a string before being displayed.
+
+'''
+
+print("I Love Python")
