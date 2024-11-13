8bcf996bea5a99d3345ec2cd29f87c176940396e
Johns Jose <jjkanichukattu@gmail.com>
2024-11-13 22:28:19 +0530

    Add files via upload

---
+++ Day-2/Johns_Jose_Task2.py
@@ -0,0 +1,14 @@
+'''
+Author:Johns Jose
+Date:13/11/2024
+Description: In this program, we will check whether 
+a given number is positive, negative, or zero.
+'''
+
+num = float(input("Enter a number: "))
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
