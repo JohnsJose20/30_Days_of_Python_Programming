9a098812d6a48ad55baed1afe92360d11fbbbd52
Kritthik2609 <kritthik2609@gmail.com>
2024-11-13 22:52:59 +0530

    Add files via upload

---
+++ Day-2/KritthikTask2.py
@@ -0,0 +1,14 @@
+"""
+Author: Kritthik
+Date: 13-11-2024
+"""
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print("The number is positive")
+
+elif num < 0:
+    print("The number is negative")
+
+else:
+    print("The number is zero")
