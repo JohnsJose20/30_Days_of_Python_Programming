4b167efd7bee60f5bb6982043a573aa16189a56c
Krishnapriya <krishnapriyaar292@gmail.com>
2024-11-14 21:16:01 +0530

    Add files via upload

---
+++ Day-3/krishnapriya_task1.py
@@ -0,0 +1,8 @@
+'''
+Author:Krishnapriya
+Date  :14/11/2024
+Description:A function that returns the number of characters in a given string
+'''
+word=str(input("Enter a word:"))
+print(len(word))
+
