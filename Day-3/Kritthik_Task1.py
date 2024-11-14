2496f4a239bfdc69112e1cf81b7f4c3a1a4f180b
Kritthik2609 <kritthik2609@gmail.com>
2024-11-14 21:34:02 +0530

    Add files via upload

---
+++ Day-3/Kritthik_Task1.py
@@ -0,0 +1,9 @@
+"""
+Author: Kritthik
+Date: 14-11-2024
+"""
+custom1 = "Hello"
+print("The length of the string is:",len(custom1))
+
+custom2 = "World"
+print("The length of the string is:",len(custom2))
