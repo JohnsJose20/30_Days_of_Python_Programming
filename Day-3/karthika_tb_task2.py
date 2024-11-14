ae2413e586b3451bd1b0b656fc894b7d8ae9f25d
karthikatb <karthikatb20@gmail.com>
2024-11-14 21:28:55 +0530

    Add files via upload

---
+++ Day-3/karthika_tb_task2.py
@@ -0,0 +1,9 @@
+'''
+author:karthika tb
+date:14-11-2024
+description:A function that combines two strings together and returns the result.
+'''
+str1=input("enter first word:")
+str2=input("enter second word:")
+word=str1+str2
+print(word)
