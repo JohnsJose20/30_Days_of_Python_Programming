b5fd72efb578addc25de993e115aaea2deaa5ff0
Krishnapriya <krishnapriyaar292@gmail.com>
2024-11-14 21:31:50 +0530

    Add files via upload

---
+++ Day-3/krishnapriya_task02.py
@@ -0,0 +1,9 @@
+'''
+Author:Krishnapriya
+Date  :14/11/2024
+Description:A function that combines two strings together and returns the result.
+'''
+word_1=str(input("Enter the first word:"))
+word_2=str(input("Enter the second word:"))
+word_3=word_1+word_2
+print(word_3)
