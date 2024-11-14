8faad89fb00659846e69f62b0abaa714aeb3bf60
joubertjose <joubertjose200614@gmail.com>
2024-11-15 00:27:10 +0530

    Add files via upload

---
+++ Day-3/joubert_task1.py
@@ -0,0 +1,5 @@
+'''
+description:A function that returns the number of characters in a given string
+'''
+word=str(input("Enter a word:"))
+print(len(word))
---
+++ Day-3/joubert_task2.py
@@ -0,0 +1,7 @@
+'''
+description:Write a function that combines two strings together and return the result
+'''
+str1=input("Enter the first word")
+str2=input("Enter the second word")
+word=str1+str2
+print(word)
