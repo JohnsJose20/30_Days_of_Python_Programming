eeacc895afc7a6f0b50d9351f2d9e515839d2031
Johns Jose <jjkanichukattu@gmail.com>
2024-11-15 00:34:51 +0530

    Add files via upload

---
+++ Day-3/Johns_Jose_Task2.py
@@ -0,0 +1,10 @@
+'''
+Author: Johns Jose
+date:14-11-2024
+description:A function that combines two 
+strings together and returns the result.
+'''
+Var_1=input("enter first word:")
+Var_2=input("enter second word:")
+Var=Var_1+Var_2
+print(Var)
