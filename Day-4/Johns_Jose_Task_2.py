2117e5e563c2208cb549736be1ccf0ed2de55da0
Johns Jose <jjkanichukattu@gmail.com>
2024-11-16 10:08:51 +0530

    Add files via upload

---
+++ Day-4/Johns_Jose_Task_2.py
@@ -0,0 +1,11 @@
+'''Author:Johns Jose
+Date  :15/11/2024
+Description:A for loop in Python is used to iterate over a sequence 
+(like a list, tuple, dictionary, string, or range) and execute a block 
+of code multiple times. The for loop is generally used when you know 
+beforehand how many times you want the loop to run.
+'''
+
+num_rows = 5
+for i in range(1, num_rows + 1):
+    print("*" * i)
