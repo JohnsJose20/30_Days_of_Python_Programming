8634e9e209605eae8ffb4ec03ee14d01967fae94
karthikatb <karthikatb20@gmail.com>
2024-11-16 11:33:53 +0530

    Add files via upload

---
+++ Day-4/karthika_tb_task2.py
@@ -0,0 +1,7 @@
+'''
+author:karthika tb
+date:15-11-2024
+description: Print a triangular pattern of stars (*) where the number of stars increases by one with each row.
+'''
+for i in range(1,6):
+    print("*"*i)
