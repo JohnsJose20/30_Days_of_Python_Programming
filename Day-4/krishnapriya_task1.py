27b39b83538dbcb0f1902b1de6bcdc7d35d5fc3e
Krishnapriya <krishnapriyaar292@gmail.com>
2024-11-16 12:24:28 +0530

    Update krishnapriya_task1.py

--- Day-4/krishnapriya_task1.py
+++ Day-4/krishnapriya_task1.py
@@ -1,3 +1,7 @@
+'''
+Author:Krishnapriya
+Date  :15/11/2024
+Description:A python program that checks if a given word is a pallindrome.A pallindrome is a word thats reads the same forward and backward
 word=input("enter a word:")
 if word==word[::-1]:
     print(word,"is a pallindrom")
