e3cc51fd58a93d520a0ef2822d1f7f68b06c9c86
Kritthik2609 <kritthik2609@gmail.com>
2024-11-16 23:52:53 +0530

    Add files via upload

---
+++ Day-5/Task_2/Kritthik_Task_2.py
@@ -0,0 +1,12 @@
+"""
+Author: Kritthik
+Date: 16-11-2024
+"""
+fruits = ["orange","apple", "cherry", "banana"]
+fruits.remove("banana")
+print(fruits)
+
+fruits.sort()
+print(fruits)
+
+print(fruits[1:3])
