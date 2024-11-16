54f810b4386e8cdf532dfcef703885e01cf22475
Krishnapriya <krishnapriyaar292@gmail.com>
2024-11-16 12:57:05 +0530

    Add files via upload

---
+++ Day-4/krishnapriya_task2.py
@@ -0,0 +1,8 @@
+'''
+Author:Krishnapriya
+Date  :15/11/2024
+Description:Print a triangular pattern of stars where the number of stars increases by one with each row
+'''
+for i in range(1,6):
+    print('*'* i)
+
