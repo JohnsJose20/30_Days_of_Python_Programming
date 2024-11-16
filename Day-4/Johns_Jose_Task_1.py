f7d4f820a2f562727afeac68f8350d6d66209994
Johns Jose <jjkanichukattu@gmail.com>
2024-11-16 10:04:07 +0530

    Add files via upload

---
+++ Day-4/Johns_Jose_Task_1.py
@@ -0,0 +1,13 @@
+'''Author:Johns Jose
+Date  :15/11/2024
+Description:This is a Python program that checks 
+if a given word is a palindrome. A palindrome is a word 
+that reads the same forward and backward
+'''
+
+word = input("Enter a word: ")
+if word == word[::-1]:
+    print(f'"{word}" is a palindrome.')
+else:
+    print(f'"{word}" is not a palindrome.')
+
