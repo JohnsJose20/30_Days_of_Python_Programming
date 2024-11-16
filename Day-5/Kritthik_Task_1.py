638c59c0961b18526cc8e0248a1f1b53ce025999
Kritthik2609 <kritthik2609@gmail.com>
2024-11-16 21:35:19 +0530

    Add files via upload

---
+++ Day-5/Kritthik_Task_1.py
@@ -0,0 +1,12 @@
+"""
+Author: Kritthik
+Date: 16-11-2024
+Description:
+"""
+color = ["Violet", "Indigo", "Blue", "Green", "Yellow", "Orange", "Red"]
+
+for i in color:
+    print(i, end = " ")
+
+color.append("Black")
+print("\nNew List:", color)
