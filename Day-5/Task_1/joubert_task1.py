827a2a657495b2ba983abd102b45d99953b553db
joubertjose <joubertjose200614@gmail.com>
2024-11-16 23:00:01 +0530

    Add files via upload

---
+++ Day-5/Task_1/joubert_task1.py
@@ -0,0 +1,10 @@
+"""
+Author: joubert
+Date: 16-11-2024
+"""
+favorite_foods = ["pizza", "sushi", "ice cream"]
+for food in favorite_foods:
+    print(food)
+favorite_foods.append("pasta")
+print("\nUpdated List:")
+print(favorite_foods)
