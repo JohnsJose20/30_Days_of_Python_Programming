36425bc88590a3e7d068321b47b6eb88ce17e2c1
joubertjose <joubertjose200614@gmail.com>
2024-11-16 23:01:30 +0530

    Add files via upload

---
+++ Day-5/Task_2/joubert_Task2.py
@@ -0,0 +1,10 @@
+"""
+Author: joubert
+Date: 16-11-2024
+"""
+fruits = ["apple", "banana", "cherry", "orange"]
+fruits.remove("banana")
+print(fruits)
+fruits.sort()
+print(fruits)
+print(fruits[1:3])
