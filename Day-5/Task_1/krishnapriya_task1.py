2ba47f1d051ebc2898427076ab30c0b239f89258
Krishnapriya <krishnapriyaar292@gmail.com>
2024-11-17 09:54:38 +0530

    Add files via upload

---
+++ Day-5/Task_1/krishnapriya_task1.py
@@ -0,0 +1,8 @@
+'''
+Author:Krishnapriya
+Date  :16/11/2024
+Description:Create and manipulate a list of favourite foods
+'''
+favourite_food=["pizza","sushi","icecream"]
+favourite_food.append("pasta")
+print(favourite_food)
