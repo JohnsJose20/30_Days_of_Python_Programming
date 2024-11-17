769bc53a319ee98b590358765af6c8c7e031bd60
karthikatb <karthikatb20@gmail.com>
2024-11-17 13:41:25 +0530

    Add files via upload

---
+++ Day-5/Task_1/karthika_tb_task1.py
@@ -0,0 +1,10 @@
+'''
+author:karthika tb
+date:16-11-2024
+description:Create and Manipulate a List of Favorite Foods
+'''
+favourite_food=["pizza","sushi","ice cream"]
+for i in favourite_food:
+    print(i)
+favourite_food.append("pasta")
+print(favourite_food)
