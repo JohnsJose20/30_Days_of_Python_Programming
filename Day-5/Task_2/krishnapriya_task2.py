0ac3c12f3e9e49791e790d5bb5f3c988057b45ef
Krishnapriya <krishnapriyaar292@gmail.com>
2024-11-17 09:55:16 +0530

    Add files via upload

---
+++ Day-5/Task_2/krishnapriya_task2.py
@@ -0,0 +1,12 @@
+'''
+Author:Krishnapriya
+Date  :16/11/2024
+Decsription:Removing,sorting,slicing
+'''
+fruits=["Apple","Banana","Cherrry","orange"]
+print(fruits)
+fruits.remove("Banana")
+print(fruits)
+fruits.sort()
+print(fruits)
+print(fruits[1:3])
