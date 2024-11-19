0685724845c3a922d33b04a12b32fbd3eb9bd7cf
joubertjose <joubertjose200614@gmail.com>
2024-11-19 22:40:46 +0530

    Add files via upload

---
+++ Day-6/Task_2/joubert_Task2.py
@@ -0,0 +1,8 @@
+"""
+Author: joubert
+Date: 19-11-2024
+"""
+names=["bullseye","woody","aliens","hamm","rex","trixie","twitch","sparks","chunk","buttercup"]
+for i in names:
+    print(i,end=" ")
+    
