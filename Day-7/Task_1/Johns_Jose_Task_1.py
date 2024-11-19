604dcfcf38d2459713c960ac4a5516cf17ca8349
Johns Jose <jjkanichukattu@gmail.com>
2024-11-19 23:01:41 +0530

    Add files via upload

---
+++ Day-7/Task_1/Johns_Jose_Task_1.py
@@ -0,0 +1,9 @@
+"""
+Author: Johns Jose
+Date: 19-11-2024
+"""
+while True:
+    name = input("Enter your name (or type 'stop' to quit): ")
+    if name.lower() == 'stop':
+        break
+    print(f"Hello, {name}!")
