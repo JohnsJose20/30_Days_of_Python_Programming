7852ef321c75fc4ceb68266fd6cf360b64dc852d
joubertjose <joubertjose200614@gmail.com>
2024-11-19 23:40:52 +0530

    Add files via upload

---
+++ Day-7/Task_1/joubert_task1.py
@@ -0,0 +1,9 @@
+"""
+Author: joubert
+Date: 19-11-2024
+"""
+while True:
+    name=input("Enter your name(or type 'stop' to quit):")
+    if name.lower()=='stop':
+        break
+    print(f"Hello, {name}!")
