e2a3566931bc7974f213ff0080e84ef7282de51d
Kritthik2609 <kritthik2609@gmail.com>
2024-11-21 21:32:07 +0530

    Add files via upload

---
+++ Day-8/Task_2/KritthikTask2.py
@@ -0,0 +1,12 @@
+"""
+Author: Kritthik
+Date: 21-11-2024
+"""
+custom = int(input("Enter a number: "))
+
+if custom > 10:
+    print("The number is greater than 10")
+elif custom < 10:
+    print("The number is less than 10")
+else:
+    print("The number is equal to 10")
