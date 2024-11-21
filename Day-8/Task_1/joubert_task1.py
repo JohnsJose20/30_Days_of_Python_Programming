6060184ff0f248b7873bd54fe92708cdd84c7a84
joubertjose <joubertjose200614@gmail.com>
2024-11-21 22:45:54 +0530

    Add files via upload

---
+++ Day-8/Task_1/joubert_task1.py
@@ -0,0 +1,11 @@
+'''
+author:joubert
+date:21-11-2024
+description: a program that checks if a number is even or odd.
+'''
+number=int(input("enter a number:"))
+if number%2==0:
+    print(number,"is an even number")
+
+else:
+    print(number,"is an odd number")
