a65e3e216023a2da653e53771c4487e788f12f73
karthikatb <karthikatb20@gmail.com>
2024-11-21 22:38:48 +0530

    Add files via upload

---
+++ Day-9/Task_2/karthika_tb_task2.py
@@ -0,0 +1,10 @@
+'''
+Author:Karthika TB
+Date:21-11-2024
+'''
+tuple1=("python", "is", "fun")
+tuple2 = ("and", "easy", "to", "learn")
+combined_tuple=tuple1+tuple2
+print(combined_tuple)
+length=len(combined_tuple)
+print(length)
