c6e6ee311fea100a828c1e725f1be9402e4ed6bf
Krishnapriya <krishnapriyaar292@gmail.com>
2024-11-21 21:55:23 +0530

    Add files via upload

---
+++ Day-8/Task_1/krishnapriya_task1.py
@@ -0,0 +1,10 @@
+'''
+Author:krishnapriya
+Date  :21/11/2024
+Description:Check whether the number is odd or even
+'''
+number=int(input("Enter a number: "))
+if number%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
