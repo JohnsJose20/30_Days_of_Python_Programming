b8c298d4c25cfa15099e9e5cd0229b52eb09d726
karthikatb <karthikatb20@gmail.com>
2024-11-21 21:08:55 +0530

    Add files via upload

---
+++ Day-8/Task_2/karthika_tb_task2.py
@@ -0,0 +1,11 @@
+'''
+Author:Karthika TB
+Date:20-11-2024
+description:A program that checks if the user's input is greater than 10 or not.
+'''
+number=int(input("enter a number:"))
+if number>10:
+    print(number,"is greater than 10 ")
+else:
+    print(number,"is less than 10")
+
