bd30174eaa72a27905ecc2ec3590c5f38f0ca546
Krishnapriya <krishnapriyaar292@gmail.com>
2024-11-21 22:51:13 +0530

    Add files via upload

---
+++ Day-9/Task_2/krishnapriya_task2.py
@@ -0,0 +1,13 @@
+'''
+Author:Krishnapriya
+Date  :21/11/2024
+Description:Tuple concatenation and length
+'''
+tuple_1=("python","is", "fun")
+tuple_2=("and","easy","to","learn")
+tuple_3=(tuple_1+tuple_2)
+print(tuple_3)
+print(len(tuple_3))
+
+
+
