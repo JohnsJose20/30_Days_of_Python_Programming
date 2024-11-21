e3111bf6f845663ef19e969185f3eccc3c4d12b9
Krishnapriya <krishnapriyaar292@gmail.com>
2024-11-21 22:01:50 +0530

    Add files via upload

---
+++ Day-8/Task_2/krishnapriya_task2.py
@@ -0,0 +1,10 @@
+'''
+Author:Krishnapriya
+Date  :21/11/2024
+Description:Check if the user's input is greater than 10 or not
+'''
+number=int(input("Enter the number"))
+if number>10:
+    print("The number is greater than 10")
+else:
+    print("The number is less than 10")
