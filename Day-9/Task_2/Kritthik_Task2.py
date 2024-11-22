5d82db909dd8da4b4446ec01a7a35b56b24b104e
Kritthik2609 <kritthik2609@gmail.com>
2024-11-22 23:17:20 +0530

    Add files via upload

---
+++ Day-9/Task_2/Kritthik_Task2.py
@@ -0,0 +1,10 @@
+"""
+Author: Kritthik
+Date: 22-11-2024
+"""
+tuple1 = ("Python", "is", "fun")
+tuple2 = ("and", "easy", "to", "learn")
+
+custom = tuple1 + tuple2
+print(custom)
+print(len(custom))
