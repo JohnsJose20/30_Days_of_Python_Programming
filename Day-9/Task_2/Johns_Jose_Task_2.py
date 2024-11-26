83ec757cbcc1a25ebbfb51b3c3f7284e97dfdb17
Johns Jose <jjkanichukattu@gmail.com>
2024-11-26 20:54:50 +0530

    Add files via upload

---
+++ Day-9/Task_2/Johns_Jose_Task_2.py
@@ -0,0 +1,10 @@
+"""
+Author: Johns Jose
+Date: 19-11-2024
+"""
+tuple1 = ("Python", "is", "fun")
+tuple2 = ("and", "easy", "to", "learn")
+
+custom = tuple1 + tuple2
+print(custom)
+print(len(custom))
