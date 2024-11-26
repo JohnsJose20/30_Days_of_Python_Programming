0b213541f15a601732333eed6044430569110687
Kritthik2609 <kritthik2609@gmail.com>
2024-11-26 23:17:22 +0530

    Add files via upload

---
+++ Day-10/Kritthik_Task_1.py
@@ -0,0 +1,9 @@
+"""
+Author: Kritthik
+Date: 26-11-2024
+"""
+custom1 = {1, 2, 3, 4, 5}
+custom2 = {4, 5, 6, 7, 8}
+
+print(f"Union of sets: {custom1|custom2}")
+print(f"Intersection of sets: {custom1&custom2}")
