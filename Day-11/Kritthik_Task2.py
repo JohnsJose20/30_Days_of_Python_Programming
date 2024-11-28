bcac6bd69b96a4276314df4ace63833fe8e41f54
Kritthik2609 <kritthik2609@gmail.com>
2024-11-28 23:49:58 +0530

    Add files via upload

---
+++ Day-11/Kritthik_Task2.py
@@ -0,0 +1,11 @@
+"""
+Author: Kritthik
+Date: 28-11-2024
+"""
+custom = (5, 10)
+a, b = custom
+print("Before swapping: a =",a, ",", "b =",b)
+
+
+b, a = custom
+print("After swapping: a =",a, ",","b =",b)
