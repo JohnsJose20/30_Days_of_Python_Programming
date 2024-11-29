6dff894ac1b27f6287c539cb7365f5bfb576e6b8
joubertjose <joubertjose200614@gmail.com>
2024-11-29 14:22:11 +0530

    Add files via upload

---
+++ Day-10/joubert_task1.py
@@ -0,0 +1,9 @@
+"""
+Author: Joubert
+Date: 29-11-2024
+"""
+custom1 = {1, 2, 3, 4, 5}
+custom2 = {4, 5, 6, 7, 8}
+
+print(f"Union of sets: {custom1|custom2}")
+print(f"Intersection of sets: {custom1&custom2}")
