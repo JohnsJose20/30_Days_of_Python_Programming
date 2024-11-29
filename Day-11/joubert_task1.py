628d41c410e0cd2903845f63e29f101a00714b80
joubertjose <joubertjose200614@gmail.com>
2024-11-29 14:31:36 +0530

    Add files via upload

---
+++ Day-11/joubert_task1.py
@@ -0,0 +1,11 @@
+"""
+Author: Joubert
+Date: 29-11-2024
+"""
+person = ("John Doe", 28, "Engineer", "New York")
+name, age, profession, city = person
+print("Name:", name)
+print("Age:", age)
+print("Profession:", profession)
+print("City:", city)
+
---
+++ Day-11/joubert_task2.py
@@ -0,0 +1,11 @@
+"""
+Author: Joubert
+Date: 29-11-2024
+"""
+custom = (5, 10)
+a, b = custom
+print("Before swapping: a =",a, ",", "b =",b)
+
+
+b, a = custom
+print("After swapping: a =",a, ",","b =",b)
